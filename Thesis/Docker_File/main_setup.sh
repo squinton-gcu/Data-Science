9ff8cdcd2d94e968f70450611d702f1ffc169818
squinton-gcu <93558295+squinton-gcu@users.noreply.github.com>
2022-08-03 21:25:51 -0700

    Add files via upload

--- Thesis/Docker_File/main_setup.sh
+++ Thesis/Docker_File/main_setup.sh
@@ -3,20 +3,20 @@
 #This script will run the entire pipeline in one go
 #It will be deployed in the Docker Image
 
-Rscript Processing_Module.r
+Rscript application/Processing_Module.r
 
 sleep 5
 
-python feature_selection_module.py 
+python application/feature_selection_module.py 
 
 sleep 5
 
-python correlation_module.py 
+python application/correlation_module.py 
 
 sleep 5
 
-python predictive_analysis_module.py 
+python application/predictive_analysis_module.py 
 
 sleep 5
 
-Rscript App.R 
+bash application/shiny-server.sh
