a144c352e3ed2520c3773981c3d15fde9bb947d1
squinton-gcu <93558295+squinton-gcu@users.noreply.github.com>
2022-08-09 21:08:33 -0700

    Delete Thesis/Docker_File directory

--- Thesis/Docker_File/launch.r
+++
@@ -1,1 +0,0 @@
-shiny::runApp('application/shiny_app_test', host='0.0.0.0', port=3838)
--- Thesis/Docker_File/shiny-server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p /var/log/image_test
-chown shiny.shiny /var/log/image_test
-
-exec shiny-server >> /var/log/image_test.log 2>&1
