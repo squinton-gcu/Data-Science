9edf2659e127bd437b8ab806e2d96828adc73ab9
squinton-gcu <93558295+squinton-gcu@users.noreply.github.com>
2022-08-16 20:43:05 -0700

    Add files via upload

--- Thesis/Docker_Package/scripts/app.R
+++ Thesis/Docker_Package/scripts/app.R
@@ -20,6 +20,8 @@ plasma_all_stress_cor[,1]<- str_replace_all(plasma_all_stress_cor[,1], ",", ".")
 plasma_all_stress_cor[,1]<- str_replace_all(plasma_all_stress_cor[,1], " ", ".")
 plasma_all_stress_cor[,1]<- gsub("[+]", ".",plasma_all_stress_cor[,1])
 plasma_all_stress_cor[,1]<- gsub("[()]", ".",plasma_all_stress_cor[,1])
+plasma_all_stress_cor[,1]<- gsub("[[]]", ".",plasma_all_stress_cor[,1])
+plasma_all_stress_cor[,1]<- gsub("\\[|\\]", ".",plasma_all_stress_cor[,1])
 csf_all_stress_graph <- as.data.frame(read.table("csf_all_stress_graph.csv", header=TRUE, sep=","))
 csf_all_stress_cor <- as.data.frame(read.table("sort_stress_top_all_csf_cor.csv", header=TRUE, sep=","))
 csf_all_stress_cor[,1]<- str_replace_all(csf_all_stress_cor[,1], "-", ".")
@@ -27,16 +29,43 @@ csf_all_stress_cor[,1]<- str_replace_all(csf_all_stress_cor[,1], ",", ".")
 csf_all_stress_cor[,1]<- str_replace_all(csf_all_stress_cor[,1], " ", ".")
 csf_all_stress_cor[,1]<- gsub("[()]", ".",csf_all_stress_cor[,1])
 csf_all_stress_cor[,1]<- gsub("[+]", ".",csf_all_stress_cor[,1])
+csf_all_stress_cor[,1]<- gsub("[[]]", ".",csf_all_stress_cor[,1])
+csf_all_stress_cor[,1]<- gsub("\\[|\\]", ".",csf_all_stress_cor[,1])
 colnames(csf_all_stress_graph) = str_replace_all(colnames(csf_all_stress_graph), "-", ".")
 colnames(csf_all_stress_graph) = str_replace_all(colnames(csf_all_stress_graph), ",", ".")
 colnames(csf_all_stress_graph) = str_replace_all(colnames(csf_all_stress_graph), " ", ".")
 colnames(csf_all_stress_graph) = gsub("[()]", ".",colnames(csf_all_stress_graph))
 colnames(csf_all_stress_graph) = gsub("[+]", ".",colnames(csf_all_stress_graph))
+colnames(csf_all_stress_graph) = gsub("[[]]", ".",colnames(csf_all_stress_graph))
+colnames(csf_all_stress_graph) =  gsub("\\[|\\]", ".",colnames(csf_all_stress_graph))
 colnames(plasma_all_stress_graph) = str_replace_all(colnames(plasma_all_stress_graph), "-", ".")
 colnames(plasma_all_stress_graph) = str_replace_all(colnames(plasma_all_stress_graph), ",", ".")
 colnames(plasma_all_stress_graph) = str_replace_all(colnames(plasma_all_stress_graph), " ", ".")
 colnames(plasma_all_stress_graph) = gsub("[()]", ".",colnames(plasma_all_stress_graph))
 colnames(plasma_all_stress_graph) = gsub("[+]", ".",colnames(plasma_all_stress_graph))
+colnames(plasma_all_stress_graph) = gsub("[[]]", ".",colnames(plasma_all_stress_graph))
+colnames(plasma_all_stress_graph) = gsub("\\[|\\]", ".",colnames(plasma_all_stress_graph))
+
+#load other data files
+ALZPProcessed <- read.table("ALZ_plasma_processed.csv", header=TRUE, sep=",")
+ALZCProcessed <- read.table("ALZ_csf_processed.csv", header=TRUE, sep=",")
+TraumaHProcessed <- read.table("trauma_human_processed.csv", header=TRUE, sep=",")
+TraumaRProcessed <- read.table("trauma_rat_processed.csv", header=TRUE, sep=",")
+
+ALZPTP <- read.table("ALZ_plasma_tp-scores.csv", header=TRUE, sep=",")
+ALZCTP <- read.table("ALZ_csf_tp-scores.csv", header=TRUE, sep=",")
+THTP <- read.table("trauma_human_tp-scores.csv", header=TRUE, sep=",")
+TRTP <- read.table("trauma_rat_tp-scores.csv", header=TRUE, sep=",")
+
+TSAP <- read.table("sort_stress_top_all_plasma_cor.csv", header=TRUE, sep=",")
+TSTP <- read.table("sort_stress_top_top_plasma_cor.csv", header=TRUE, sep=",")
+TSAC <- read.table("sort_stress_top_all_csf_cor.csv", header=TRUE, sep=",")
+TSTC <- read.table("sort_stress_top_top_csf_cor.csv", header=TRUE, sep=",")
+
+TSAPP <- read.table("stress_top_plasma_all_pred.csv", header=TRUE, sep=",")
+TSTPP <- read.table("stress_top_plasma_top_pred.csv", header=TRUE, sep=",")
+TSCAP <- read.table("stress_top_csf_all_pred.csv", header=TRUE, sep=",")
+TSTCP <- read.table("stress_top_csf_top_pred.csv", header=TRUE, sep=",")
 
 plasma_all_stress_pred <- as.data.frame(read.table("stress_top_plasma_all_pred.csv", header=TRUE, sep=","))
 csf_all_stress_pred<- as.data.frame(read.table("stress_top_csf_all_pred.csv", header=TRUE, sep=","))
@@ -93,63 +122,42 @@ ui <- mainPanel(titlePanel("The Investigation of Alzheimer's disease and Stress 
                          htmlOutput("text"),
                          dataTableOutput("table2")))),
                         
-                        # tabPanel("Processing Results", fluidRow(
-                          # column(3,
-                            # verticalLayout(
-                              # textOutput("text1"),
-                              # imageOutput("ALZPlasmaN1"),
-                              # imageOutput("ALZPlasmaN2"),
-                              # imageOutput("ALZPlasmaS1"),
-                              # imageOutput("ALZPlasmaS2")
-                            # )),
-                          # column(3,
-                            # verticalLayout(
-                              # textOutput("text2"),
-                              # imageOutput("ALZCSFN1"),
-                              # imageOutput("ALZCSFN2"),
-                              # imageOutput("ALZCSFS1"),
-                              # plotOutput("ALZCSFS2")
-                            # )),
-                          # column(3,
-                            # verticalLayout(
-                              # textOutput("text3"),
-                              # imageOutput("stressHN1"),
-                              # imageOutput("stressHN2"),
-                              # imageOutput("stressHS1"),
-                              # imageOutput("stressHS2")
-                            # )),
-                          # column(3,
-                            # verticalLayout(
-                              # textOutput("text4"),
-                              # imageOutput("stressRN1"),
-                              # imageOutput("stressRN2"),
-                              # imageOutput("stressRS1"),
-                              # imageOutput("stressRS2")
-                            # ))
-                        # )),
                         tabPanel("Prediction Maps", fluidRow(
                           column(3,
-                            verticalLayout(
-                              textOutput("text5"),
-                              imageOutput("MatrixPlasma1"),
-                              imageOutput("MatrixPlasma2"),
-                              imageOutput("MatrixPlasma3")
-                            )),
+                                 verticalLayout(
+                                   textOutput("text5"),
+                                   imageOutput("MatrixPlasma1"),
+                                   imageOutput("MatrixPlasma2"),
+                                   imageOutput("MatrixPlasma3")
+                                 )),
                           column(3,
-                            verticalLayout(
-                              textOutput("text6"),
-                              imageOutput("MatrixPlasmaT1")
-                            )),
+                                 verticalLayout(
+                                   textOutput("text6"),
+                                   imageOutput("MatrixPlasmaT1")
+                                 )),
                           column(3,
-                            verticalLayout(
-                              textOutput("text7"),
-                              imageOutput("MatrixCSF1"),
-                              imageOutput("MatrixCSF2"),
-                              imageOutput("MatrixCSF3")
+                                 verticalLayout(
+                                   textOutput("text7"),
+                                   imageOutput("MatrixCSF1"),
+                                   imageOutput("MatrixCSF2"),
+                                   imageOutput("MatrixCSF3")
                             ))
-                        )))
-                )
-
+                        )),
+                        tabPanel("Download Data", titlePanel("Download Data"),
+                              sidebarPanel(
+                                selectInput("dataset", "Choose a dataset:", choices = c("ALZPlasmaProcessed", "ALZCSFProcessed", "TraumaHumanProcessed", "TraumaRatProcessed",
+                                                                                        "ALZPlasmaTvaluePvalue", "ALZCSFTvaluePvalue", "TraumaHumanTvaluePvalue", "TraumaRatTvaluePvalue",
+                                                                                        "TopStressAllPlasma", "TopStressTopPlasma", "TopStressAllCSF", "TopStressTopCSF",
+                                                                                        "TopStressAllPlasmaPredictions", "TopStressTopPlasmaPredictions", "TopStressAllCSFPredictions", "TopStressTopCSFPredictions")),
+                                downloadButton("downloadData", "Download")
+                              ),
+                              mainPanel(
+                                tableOutput("tableTest"))
+                              )
+                              )
+                      
+                
+  )
 )
 
 # Define server logic to plot ---
@@ -241,21 +249,15 @@ server <- function(input, output, session) {
   
   setwd(location)
   location
-  # output$ALZPlasmaN1 <- renderImage({list(src = "/application/application/application/ALZ_plasma _normalized1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  # output$ALZPlasmaN2 <- renderImage({list(src = "/application/application/application/ALZ_plasma _normalized2.png", contentType="image/png", width = 400, height = 500)}, deleteFile = F)
-  # output$ALZPlasmaS1 <- renderImage({list(src = "/application/application/application/ALZ_plasma _scale1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  # output$ALZPlasmaS2 <- renderImage({list(src = "/application/application/application/ALZ_plasma _scale2.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  
+  # output$ALZPlasmaN1 <- renderImage({list(src = "ALZ_plasma _normalized1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
+  # output$ALZPlasmaN2 <- renderImage({list(src = "ALZ_plasma _normalized2.png", contentType="image/png", width = 400, height = 500)}, deleteFile = F)
+
   # output$ALZCSFN1 <- renderImage({list(src = "/application/application/application/ALZ_csf _normalized1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
   # output$ALZCSFN1 <- renderImage({list(src = "/application/application/application/ALZ_csf _normalized2.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  # output$ALZCSFS1 <- renderImage({list(src = "/application/application/application/ALZ_csf _scale1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  # output$ALZCSFS2 <- renderImage({list(src = "/application/application/application/ALZ_csf _scale2.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  
+
   # output$stressHN1 <- renderImage({list(src = "/application/application/application/trauma_human _normalized1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
   # output$stressHN2 <- renderImage({list(src = "/application/application/application/trauma_human _normalized2.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  # output$stressHS1 <- renderImage({list(src = "/application/application/application/trauma_human _scale1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  # output$stressHS2 <- renderImage({list(src = "/application/application/application/trauma_human _scale1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
-  
+
   # output$stressRN1 <- renderImage({list(src = "/application/application/application/trauma_rat _normalized1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
   # output$stressRN2 <- renderImage({list(src = "/application/application/application/trauma_rat _normalized2.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
   # output$stressRS1 <- renderImage({list(src = "/application/application/application/trauma_rat _scale1.png", contentType="image/png", width = 400, height = 400)}, deleteFile = F)
@@ -279,6 +281,41 @@ server <- function(input, output, session) {
   output$text5 <- renderText( { "ALZ Plasma all "})
   output$text6 <- renderText( {"ALZ Plasma top"})
   output$text7 <- renderText({"ALZ CSF All"})
+  
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "ALZPlasmaProcessed" = ALZPProcessed,
+           "ALZCSFProcessed" =ALZCProcessed,
+           "TraumaHumanProcessed" =TraumaHProcessed,
+           "TraumaRatProcessed" =TraumaRProcessed,
+           
+           "ALZPlasmaTvaluePvalue" =ALZPTP,
+           "ALZCSFTvaluePvalue" =ALZCTP,
+           "TraumaHumanTvaluePvalue" =THTP,
+           "TraumaRatTvaluePvalue" =TRTP,
+           "TopStressAllPlasma" =TSAP,
+           "TopStressTopPlasma" =TSTP,
+           "TopStressAllCSF" =TSAC,
+           "TopStressTopCSF" =TSTC,
+           "TopStressAllPlasmaPredictions" = TSAPP,
+           "TopStressTopPlasmaPredictions" =TSTPP,
+           "TopStressAllCSFPredictions" =TSCAP,
+           "TopStressTopCSFPredictions" =TSTCP)
+  })
+  
+  output$tableTest <- renderTable({
+    datasetInput()
+  })
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste(input$dataset, ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(datasetInput(), file, row.names = FALSE)
+    
+    })
+  
 }
 
 #runGadget(ui, server, viewer = browserViewer(browser = getOption("browser")))
